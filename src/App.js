215fa72b992ad7aeacd1619e4923d8ed4df3bd19
ChenZhiPeng <634323580@qq.com>
2020-11-16 18:29:34 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Header from './common/header'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
 import store from './store'
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
+      <div>
+        <Header/>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/detail" exact component={Detail}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
